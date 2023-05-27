8416d53859471c91cb0ce3354875682af7e10c51
Mini <minnnidev@gmail.com>
2023-05-28 00:23:56 +0900

    [Setting] 프로젝트 생성

---
+++ BookReview/BookReview/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BookReview
+//
+//  Created by 김민 on 2023/05/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
