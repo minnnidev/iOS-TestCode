9e8120f1e883ffe755d0be8b93512917eee70573
Mini <minnnidev@gmail.com>
2023-05-30 01:00:37 +0900

    [Chore] #12 - .gitignore 수정

--- BookReview/BookReview/Manager/BookSearchManager.swift
+++ BookReview/BookReview/Manager/BookSearchManager.swift
@@ -11,13 +11,18 @@ import Alamofire
 
 struct BookSearchManager {
     
+    private let id = Bundle.main.object(forInfoDictionaryKey: "X_Naver_Client_Id") as? String
+    private let secret = Bundle.main.object(forInfoDictionaryKey: "X_Naver_Client_Secret") as? String
+    
     func request(from keyword: String, completionHandler: @escaping (([Book]) -> Void)) {
         guard let url = URL(string: "https://openapi.naver.com/v1/search/book.json") else { return }
+        guard let id = id else { return }
+        guard let secret = secret else { return }
         let parameters = BookSearchRequestModel(query: keyword)
         
         let headers: HTTPHeaders = [
-            "X-Naver-Client-Id": Constant().naverClientId,
-            "X-Naver-Client-Secret": Constant().naverClientSecret
+            "X-Naver-Client-Id": id,
+            "X-Naver-Client-Secret": secret
         ]
         
         AF.request(url, method: .get, parameters: parameters, headers: headers)
